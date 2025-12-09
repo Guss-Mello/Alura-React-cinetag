4fe23495b0adfffb19a0311781c43d3aca92202a
Gustavo Mello Soares dos Santos <gustavo@digicom.com.br>
2025-12-09 18:34:37 -0300

    Pagina de Nao Encotrada criada!

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -3,6 +3,7 @@ import styles from './Player.module.css';
 import Titulo from 'components/Titulo';
 import videos from 'json/db.json';
 import { useParams } from 'react-router-dom';
+import NaoEncontrada from 'pages/NaoEncontrada/lindex';
 
 
 function Player() {
@@ -10,6 +11,11 @@ function Player() {
     const video = videos.find((video) => {
         return video.id === Number(paramentros.id)
     })
+
+    if (!video) {
+        return <NaoEncontrada />
+    }
+
     return (
         <>
             <Banner imagem="player" />
